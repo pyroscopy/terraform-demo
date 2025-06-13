82437986bf4d7d3859e6042f1132aeb88d731ca2
pyroscopy <devsecuops@gmail.com>
2025-06-14 00:56:30 +0900

    fix: remove s3 code in ec2 module

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,13 +1,3 @@
 output "instance_id" {
   value = aws_instance.this.id
 }
-
-output "bucket_id" {
-  description = "생성된 S3 버킷의 ID"
-  value       = aws_s3_bucket.my_bucket.id
-}
-
-output "bucket_arn" {
-  description = "생성된 S3 버킷의 ARN"
-  value       = aws_s3_bucket.my_bucket.arn
-} 
